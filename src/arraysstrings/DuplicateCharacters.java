85fd854aca84fac6b207f0d04759bded6822b480
Gulzhazira Tolaganova <gulzhazira@macbookair.myfiosgateway.com>
2025-04-23 20:39:28 -0400

    Duplicats

--- src/arraysstrings/DuplicateCharacters.java
+++ src/arraysstrings/DuplicateCharacters.java
@@ -3,19 +3,16 @@ package arraysstrings;
 public class DuplicateCharacters {
 
     public static void main(String[] args){
-        String str = "america";
-        int[] counts = new int[str.length()];
-        for(int i = 0; i < str.length()-1; i++){
-            for(int j = 1; j < str.length(); j++){
-                if(str.charAt(i) == str.charAt(j)){
-                    counts[str.charAt(i)]++;
-                }
-            }
+        String str = "jazirajazi";
+        int[] n = new int[256];
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            n[ch]++;
         }
 
-        for(int i = 0; i< counts.length; i++){
-            if(counts[i]>1){
-                System.out.println((char)i + ": " + counts[i]);
+        for(int i = 0; i< n.length; i++){
+            if(n[i]>1){
+                System.out.println((char)i + ": " + n[i]);
             }
         }
     }
